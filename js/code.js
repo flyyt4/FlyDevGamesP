110a8c6b88c8b7cad607bea45398a064175f0a06
flyyt4 <98108996+flyyt4@users.noreply.github.com>
2022-06-27 17:04:25 -0300

    add pg

---
+++ js/code.js
@@ -0,0 +1,21 @@
+//creador/creator https://github.com/EddyZxQ
+
+document.addEventListener("keyup", e => {
+
+    if (e.target.matches("#buscador")) {
+
+        if (e.key === "Escape") e.target.value = ""
+
+        document.querySelectorAll(".articulo").forEach(fruta => {
+
+            fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase()) ?
+                fruta.classList.remove("filtro") :
+                fruta.classList.add("filtro")
+        })
+
+    }
+
+
+})
+
+//mi codigo/my code
